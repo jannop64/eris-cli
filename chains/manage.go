74285f8c594a53457d978516327f3849fdf08506
Casey Kuhlman <casey@erisindustries.com>
2016-08-26 18:19:31 +0200

    DO NOT MERGE; contemplated changes necessary to use cm as a lib in cli

--- chains/manage.go
+++ chains/manage.go
@@ -7,10 +7,10 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
+	// "strings"
 
 	"github.com/eris-ltd/eris-cli/config"
-	"github.com/eris-ltd/eris-cli/data"
+	// "github.com/eris-ltd/eris-cli/data"
 	"github.com/eris-ltd/eris-cli/definitions"
 	"github.com/eris-ltd/eris-cli/loaders"
 	"github.com/eris-ltd/eris-cli/perform"
@@ -21,6 +21,10 @@ import (
 	. "github.com/eris-ltd/common/go/common"
 	"github.com/eris-ltd/common/go/ipfs"
 	log "github.com/eris-ltd/eris-logger"
+	cm_definitions "github.com/eris-ltd/eris-cm/definitions"
+	cm_maker "github.com/eris-ltd/eris-cm/maker"
+	cm_util "github.com/eris-ltd/eris-cm/util"
+	keys "github.com/eris-ltd/eris-cm/Godeps/_workspace/src/github.com/eris-ltd/eris-keys/eris-keys"
 )
 
 // MakeChain runs the `eris-cm make` command in a docker container.
@@ -30,6 +34,7 @@ import (
 //
 //  do.Name          - name of the chain to be created (required)
 //  do.Known         - will use the mintgen tool to parse csv's and create a genesis.json (requires do.ChainMakeVals and do.ChainMakeActs) (optional)
+//  do.Output        - [csk] don't remember what this does
 //  do.ChainMakeVals - csv file to use for validators (optional)
 //  do.ChainMakeActs - csv file to use for accounts (optional)
 //  do.AccountTypes  - use eris-cm make account-types paradigm (example: Root:1,Participants:25,...) (optional)
@@ -40,93 +45,60 @@ import (
 //  do.Debug         - debug output (optional)
 //
 func MakeChain(do *definitions.Do) error {
+	// precursor functions
 	if err := checkKeysRunningOrStart(); err != nil {
 		return err
 	}
-
-	do.Service.Name = do.Name
-	do.Service.Image = path.Join(config.GlobalConfig.Config.ERIS_REG_DEF, config.GlobalConfig.Config.ERIS_IMG_CM)
-	do.Service.User = "eris"
-	do.Service.AutoData = true
-	do.Service.Links = []string{fmt.Sprintf("%s:%s", util.ServiceContainerName("keys"), "keys")}
-	do.Service.Environment = []string{
-		fmt.Sprintf("ERIS_KEYS_PATH=http://keys:%d", 4767), // note, needs to be made aware of keys port...
-		fmt.Sprintf("ERIS_CHAINMANAGER_ACCOUNTTYPES=%s", strings.Join(do.AccountTypes, ",")),
-		fmt.Sprintf("ERIS_CHAINMANAGER_CHAINTYPE=%s", do.ChainType),
-		fmt.Sprintf("ERIS_CHAINMANAGER_TARBALLS=%v", do.Tarball),
-		fmt.Sprintf("ERIS_CHAINMANAGER_ZIPFILES=%v", do.ZipFile),
-		fmt.Sprintf("ERIS_CHAINMANAGER_OUTPUT=%v", do.Output),
-		fmt.Sprintf("ERIS_CHAINMANAGER_VERBOSE=%v", do.Verbose),
-		fmt.Sprintf("ERIS_CHAINMANAGER_DEBUG=%v", do.Debug),
+	// loop through chains directories to make sure they exist & are appropriately populated
+	for _, d := range ChainsDirs {
+		if _, err := os.Stat(d); os.IsNotExist(err) {
+			os.MkdirAll(d, 0755)
+		}
+	}
+	if err := cm_util.CheckDefaultTypes(AccountsTypePath, "account-types"); err != nil {
+		return err
+	}
+	if err := cm_util.CheckDefaultTypes(ChainTypePath, "chain-types"); err != nil {
+		return err
 	}
 
-	do.Operations.ContainerType = definitions.TypeService
-	do.Operations.SrvContainerName = util.ServiceContainerName(do.Name)
-	do.Operations.DataContainerName = util.DataContainerName(do.Name)
-	do.Operations.Labels = util.Labels(do.Name, do.Operations)
-	if do.RmD {
-		do.Operations.Remove = true
-	}
+	// announce.
+	log.Info("Hello! I'm the marmot who makes eris chains.")
+	maker := cm_definitions.NowDo()
+	keys.DaemonAddr = "http://172.17.0.2:4767" // tmp
 
+	// todo. clean this up... struct merge them or something
+	maker.Name = do.Name
+	maker.Verbose = do.Verbose
+	maker.Debug = do.Debug
+	maker.ChainType = do.ChainType
+	maker.AccountTypes = do.AccountTypes
+	maker.Zip = do.ZipFile
+	maker.Tarball = do.Tarball
+	maker.Output = do.Output
 	if do.Known {
-		log.Debug("Using MintGen rather than eris:cm")
-		do.Service.EntryPoint = "mintgen"
-		do.Service.Command = fmt.Sprintf("known %s --csv=%s,%s > %s", do.Name, do.ChainMakeVals, do.ChainMakeActs, path.Join(ErisContainerRoot, "chains", do.Name, "genesis.json"))
-	} else {
-		log.Debug("Using eris:cm rather than MintGen")
-		do.Service.EntryPoint = fmt.Sprintf("eris-cm make %s", do.Name)
+		maker.CSV = fmt.Sprintf("%s,%s", do.ChainMakeVals, do.ChainMakeActs)
 	}
 
-	if !do.Known && len(do.AccountTypes) == 0 && do.ChainType == "" {
-		do.Operations.Interactive = true
-		do.Operations.Args = strings.Split(do.Service.EntryPoint, " ")
-	}
-
-	if do.Known {
-		do.Operations.Args = append(do.Operations.Args, strings.Split(do.Service.Command, " ")...)
-		do.Service.WorkDir = path.Join(ErisContainerRoot, "chains", do.Name)
-	}
-
-	doData := definitions.NowDo()
-	doData.Name = do.Name
-
-	doData.Operations.DataContainerName = util.DataContainerName(do.Name)
-	doData.Operations.ContainerType = "service"
-
-	doData.Source = AccountsTypePath
-	doData.Destination = path.Join(ErisContainerRoot, "chains", "account-types")
-	if err := data.ImportData(doData); err != nil {
+	// make it
+	if err := cm_maker.MakeChain(maker); err != nil {
 		return err
 	}
 
-	doData.Source = ChainTypePath
-	doData.Destination = path.Join(ErisContainerRoot, "chains", "chain-types")
-	if err := data.ImportData(doData); err != nil {
-		return err
+	// cm currently is not opinionated about its writers.
+	if maker.Tarball {
+		if err := cm_util.Tarball(maker); err != nil {
+			return err
+		}
+	} else if maker.Zip {
+		if err := cm_util.Zip(maker); err != nil {
+			return err
+		}
 	}
-
-	chnPath := filepath.Join(ChainsPath, do.Name)
-	doData.Source = chnPath
-	doData.Destination = path.Join(ErisContainerRoot, "chains", do.Name)
-	if err := data.ImportData(doData); err != nil {
-		return err
-	}
-
-	buf, err := perform.DockerExecService(do.Service, do.Operations)
-	if err != nil {
-		return err
-	}
-
-	io.Copy(config.GlobalConfig.Writer, buf)
-
-	doData.Source = path.Join(ErisContainerRoot, "chains")
-	doData.Destination = ErisRoot
-	if err := data.ExportData(doData); err != nil {
-		return err
-	}
-
-	if !do.RmD {
-		return data.RmData(doData)
+	if maker.Output {
+		if err := cm_util.SaveAccountResults(maker); err != nil {
+			return err
+		}
 	}
 
 	return nil
